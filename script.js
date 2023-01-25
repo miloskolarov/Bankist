dbb8d9e212d30cf1422e0eaa488c88f9bdbb4bf9
miloskolarov <milos.v.kolarov@gmail.com>
2023-01-25 10:15:21 +0100

    Fix slider

--- script.js
+++ script.js
@@ -68,12 +68,14 @@ const displayMovements = function (movements, sort = false) {
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
-    const html = `     
-  <div class="movements">
-    <div class="movements__row">
-      <div class="movements__type movements__type--${type}">${i + 1}</div>
-      <div class="movements__value">${mov}EUR</div>
-  </div>
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
     `;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
